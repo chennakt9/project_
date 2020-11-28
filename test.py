9c0282db05041607b091393095439feea38cf42c
Anitha9978 <dharavath.anitha@iitgn.ac.in>
2020-11-28 15:34:07 +0530

    searched completed

--- test.py
+++ test.py
@@ -1,4 +1,22 @@
-arr = ["abc" , "cde" , "efg"]
+# arr = ["abc" , "cde" , "efg"]
+# print(arr)
+# t = "-".join(arr)
+# print(t)
+
+timeline = [["Status 1","time1","public"],["Status 2","time2","private"],["Status 3","time3","strictly_private"]]
+
+# print(timeline[0],"sadfsad")
+
+arr = []
+for i in timeline:
+	# print(i)
+	if i[2]=='public' or  i[2]=='private':
+
+		string = "   ".join(i)
+		arr.append(string)
+
 print(arr)
-t = "-".join(arr)
-print(t)
+
+# prof = " view the profile details \n--------------------------------------------\n" + "\n".join(arr) + "\n--------------------------------------------"
+# print(prof)
+
